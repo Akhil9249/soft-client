9add0e58921faa14e7be68daf44b42149cb2206e
Akhil CV <akhilcv430@gmail.com>
2025-10-24 10:10:09 +0530

    404 error fixed

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  },
   // server: {
   //  host:'192.168.1.33',
   //  port:5173
